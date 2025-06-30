9c5b514faddb3344b10edcc4e799e34743bfd5a6
Sahil Digraskar <165980743+Skyy777@users.noreply.github.com>
2025-06-30 12:01:00 +0530

    Update Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -1,20 +1,14 @@
-// ...existing code...
 class Cat extends Animal {
-    // Implementing the abstract method
     void sound() {
         System.out.println("Meow");
     }
 }
-
-// Main class to test the abstract class and its subclasses
 public class Abstract {
     public static void main(String[] args) {
         Animal dog = new Dog();
         Animal cat = new Cat();
-
         dog.sound();
         dog.eat();
-
         cat.sound();
         cat.eat();
     }
